6e175936f72c154b5f42319e8cc65e951f8cb120
Akulapavankumar <39512266+Akulapavankumar@users.noreply.github.com>
2018-05-29 12:35:50 +0530

    Add files via upload

---
+++ YouTubeChannelPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using YouTubeEmbed.Models;
+
+namespace YouTubeEmbed.Pages
+{
+    public partial class YouTubeChannelPage : ContentPage
+    {
+        public YouTubeChannelPage()
+        {
+            InitializeComponent();
+
+
+        }   
+
+        public void PlayVideo(object sender, SelectedItemChangedEventArgs e)
+        {
+            var video = e.SelectedItem as YouTubeItem;
+            
+            if (video.VideoId != null)
+            {
+                Device.OpenUri(new Uri("https://www.youtube.com/watch?v=" + video.VideoId));
+            }
+            else
+            {
+                DisplayAlert("Pavan","ID Is Null !", "ok");
+            }
+        }
+    }
+}
